366425946add99f778333da61e198ef6ea04a9fb
Amy Trent <amytrent@Mac.lan>
2026-02-17 18:14:57 -0800

    Colors and text

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ const satisfy = Satisfy({ subsets: ['latin'], weight: '400' });
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black font-sans p-8">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-blue-100 font-sans p-8">
       
 
       {/* Heading */}
-      <h1 className={`${satisfy.className} text-4xl font-bold text-black dark:text-zinc-50 mb-4 text-center`}>
+      <h1 className={`${satisfy.className} text-4xl font-bold text-black mb-4 text-center`}>
         Hey, I'm Amy Trent
       </h1>
 
       {/* Description */}
-      <p className="text-lg text-zinc-600 dark:text-zinc-400 text-center max-w-md mb-8">
+      <p className="text-lg text-zinc-600 text-center max-w-md mb-8">
         Woohoo
       </p>
 
@@ -24,13 +24,13 @@ export default function Home() {
           href="#"
           className="px-6 py-3 rounded-full bg-black text-white hover:bg-zinc-800 transition"
         >
-          Button 1
+          Resume
         </a>
         <a
           href="#"
           className="px-6 py-3 rounded-full border border-black text-black hover:bg-black hover:text-white transition"
         >
-          Button 2
+          Portfolio
         </a>
       </div>
     </div>
